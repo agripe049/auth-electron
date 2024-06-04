5bd5dec784530e701feb9a75ffac6af179afc02d
agripe049 <agripeheber@gmail.com>
2024-06-03 22:14:01 -0300

    atualizado

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Header from './components/Header';
 
-export default function App() {
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/LoginForm';
+import Dashboard from './components/DashBoard';
+import ProtectedRoute from './RotaProtegida/ProtectedRoute';
+
+
+function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-    <div>
-
+    <Router>
       <Routes>
-          <Route  path="/" element={<Home />}></Route>
-          <Route  path="/login" element={<Login />}></Route>
-          <Route  path="/register" element={<Register />}></Route>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
       </Routes>
-    </div>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
